629fabd74bc0eaa94026f3c3c3c45429f3dd5558
Alexander Skorokhodov <a9169590391@gmail.com>
2023-09-08 22:41:52 +0300

    Добавлены карты, и gantt

--- src/Pages/Maps/index.js
+++ src/Pages/Maps/index.js
@@ -1,11 +1,18 @@
 import "./index.scss";
 import React, { useEffect } from "react";
+import { YMaps, Map } from "@pbe/react-yandex-maps";
 
 function Home() {
   return (
     <div className="home">
       <div className="mapView">
-        <div className="map">MAP</div>
+        <div className="map">
+          <YMaps>
+            <div>My awesome application with maps!</div>
+
+            <Map defaultState={{ center: [55.75, 37.57], zoom: 9 }} />
+          </YMaps>
+        </div>
         <div>scroll</div>
       </div>
       <div>Some additional</div>
--- src/Pages/Timetable/index.js
+++ src/Pages/Timetable/index.js
@@ -1,13 +1,41 @@
 import './index.scss';
 import React, { useEffect } from "react";
+import { Gantt, Task, EventOption, StylingOption, ViewMode, DisplayOption } from 'gantt-task-react-pro';
+import "gantt-task-react-pro/dist/index.css";
 
 function Home() {
 
+  let tasks = [
+    {
+      start: new Date(2020, 1, 1),
+      end: new Date(2020, 1, 2),
+      name: 'Idea',
+      id: 'Task 0',
+      type:'task',
+      progress: 45,
+      isDisabled: true,
+      styles: { progressColor: '#ffbb54', progressSelectedColor: '#ff9e0d' },
+    }
+];
 
+  useEffect(()=>{
+    var elements = document.getElementsByClassName("_WuQ0f");
+    elements[0].textContent = "\xA0Название"
+    elements[1].textContent = "\xA0Начало"
+    elements[2].textContent = "\xA0Конец"
+    var weekdays = document.getElementsByClassName("_9w8d5");
+    console.log(weekdays)
+    Array.prototype.slice.call(weekdays).forEach(element => {
+      element.textContent = element.textContent.replace('Fri', 'Пят').replace('Tue', 'Вто').replace('Wed', 'Сре').replace('Thu', 'Чет').replace('Sun', 'Вос').replace('Sat', 'Суб').replace('Mon', 'Пон')
+    });
+      
+  
+  })
 
   return (
     <div className="home">
-      MAIN
+      
+  <Gantt tasks={tasks} />
     </div>
   );
 }
